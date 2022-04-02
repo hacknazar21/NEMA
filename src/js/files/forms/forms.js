67e8246142e516cd581529d37c5a0d8b16d8e2c9
Nazar Kultayev <nazarcool2001@gmail.com>
2022-04-02 20:44:23 +0500

    Главный экран и спойлеры

---
+++ src/js/files/forms/forms.js
@@ -0,0 +1,428 @@
+// Подключение функционала "Чертогов Фрилансера"
+// Подключение списка активных модулей
+import { flsModules } from "../modules.js";
+// Вспомогательные функции
+import { isMobile, _slideUp, _slideDown, _slideToggle, FLS } from "../functions.js";
+// Модуль прокрутки к блоку
+import { gotoBlock } from "../scroll/gotoblock.js";
+
+
+
+//================================================================================================================================================================================================================================================================================================================================
+/*
+Чтобы поле участвовало в валидации добавляем атрибут data-required
+Особые проверки:
+data-required="email" - вадидация E-mail
+
+Чтобы поле валидировалось при потере фокуса, 
+к атрибуту data-required добавляем атрибут data-validate
+
+Чтобы вывести текст ошибки, нужно указать его в атрибуте data-error
+
+data-popup-message - указываем селектор попапа который нужно показать после отправки формы (режимы data-ajax или data-dev) ! необходимо подключить функционал попапов в app.js
+data-ajax - отправляем данные формы AJAX запросом по адресу указанному в action методом указанным в method
+data-dev - режим разработчика - эмитируем отправку формы
+data-goto-error - прокрутить страницу к ошибке
+*/
+
+// Работа с полями формы. Добавление классов, работа с placeholder
+export function formFieldsInit(options = { viewPass: false }) {
+	// Если включено, добавляем функционал "скрыть плейсходлер при фокусе"
+	const formFields = document.querySelectorAll('input[placeholder],textarea[placeholder]');
+	if (formFields.length) {
+		formFields.forEach(formField => {
+			if (!formField.hasAttribute('data-placeholder-nohide')) {
+				formField.dataset.placeholder = formField.placeholder;
+			}
+			formField.oninput = function () {
+				formValidate.validateInput(formField);
+			}
+		});
+	}
+
+	document.body.addEventListener("focusin", function (e) {
+		const targetElement = e.target;
+		if ((targetElement.tagName === 'INPUT' || targetElement.tagName === 'TEXTAREA')) {
+			if (targetElement.dataset.placeholder) {
+				targetElement.placeholder = '';
+			}
+			if (!targetElement.hasAttribute('data-no-focus-classes')) {
+				targetElement.classList.add('_form-focus');
+				targetElement.parentElement.classList.add('_form-focus');
+			}
+			formValidate.removeError(targetElement);
+		}
+	});
+	document.body.addEventListener("focusout", function (e) {
+		const targetElement = e.target;
+		if ((targetElement.tagName === 'INPUT' || targetElement.tagName === 'TEXTAREA')) {
+			if (targetElement.dataset.placeholder) {
+				targetElement.placeholder = targetElement.dataset.placeholder;
+			}
+			if (!targetElement.hasAttribute('data-no-focus-classes')) {
+				targetElement.classList.remove('_form-focus');
+				targetElement.parentElement.classList.remove('_form-focus');
+			}
+			// Моментальная валидация
+			if (targetElement.hasAttribute('data-validate')) {
+				formValidate.validateInput(targetElement);
+			}
+		}
+	});
+
+	// Если включено, добавляем функционал "Показать пароль"
+	if (options.viewPass) {
+		document.addEventListener("click", function (e) {
+			let targetElement = e.target;
+			if (targetElement.closest('[class*="__viewpass"]')) {
+				let inputType = targetElement.classList.contains('_viewpass-active') ? "password" : "text";
+				targetElement.parentElement.querySelector('input').setAttribute("type", inputType);
+				targetElement.classList.toggle('_viewpass-active');
+			}
+		});
+	}
+}
+// Валидация форм
+export let formValidate = {
+	getErrors(form) {
+		let error = 0;
+
+		let formRequiredItems = form.querySelectorAll('*[data-required]');
+		if (formRequiredItems.length) {
+			formRequiredItems.forEach(formRequiredItem => {
+				if ((formRequiredItem.offsetParent !== null || formRequiredItem.tagName === "SELECT") && !formRequiredItem.disabled) {
+					error += this.validateInput(formRequiredItem);
+				}
+			});
+		}
+		return error;
+	},
+	validateInput(formRequiredItem) {
+		let error = 0;
+		if (formRequiredItem.dataset.required === "email") {
+			formRequiredItem.value = formRequiredItem.value.replace(" ", "");
+			if (this.emailTest(formRequiredItem)) {
+				this.addError(formRequiredItem);
+				error++;
+			} else {
+				this.removeError(formRequiredItem);
+			}
+		} else if (formRequiredItem.type === "checkbox" && !formRequiredItem.checked) {
+			this.addError(formRequiredItem);
+			error++;
+		} else {
+			if (!formRequiredItem.value) {
+				this.addError(formRequiredItem);
+				error++;
+			} else {
+				this.removeError(formRequiredItem);
+			}
+		}
+		return error;
+	},
+	addError(formRequiredItem) {
+		formRequiredItem.classList.add('_form-error');
+		formRequiredItem.parentElement.classList.add('_form-error');
+		let inputError = formRequiredItem.parentElement.querySelector('.form__error');
+		if (inputError) formRequiredItem.parentElement.removeChild(inputError);
+		if (formRequiredItem.dataset.error) {
+			formRequiredItem.parentElement.insertAdjacentHTML('beforeend', `<div class="form__error">${formRequiredItem.dataset.error}</div>`);
+		}
+	},
+	removeError(formRequiredItem) {
+		formRequiredItem.classList.remove('_form-error');
+		formRequiredItem.parentElement.classList.remove('_form-error');
+		if (formRequiredItem.parentElement.querySelector('.form__error')) {
+			formRequiredItem.parentElement.removeChild(formRequiredItem.parentElement.querySelector('.form__error'));
+		}
+	},
+	formClean(form) {
+		form.reset();
+		setTimeout(() => {
+			let inputs = form.querySelectorAll('input,textarea');
+			for (let index = 0; index < inputs.length; index++) {
+				const el = inputs[index];
+				el.parentElement.classList.remove('_form-focus');
+				el.classList.remove('_form-focus');
+				formValidate.removeError(el);
+			}
+			let checkboxes = form.querySelectorAll('.checkbox__input');
+			if (checkboxes.length > 0) {
+				for (let index = 0; index < checkboxes.length; index++) {
+					const checkbox = checkboxes[index];
+					checkbox.checked = false;
+				}
+			}
+			if (flsModules.select) {
+				let selects = form.querySelectorAll('.select');
+				if (selects.length) {
+					for (let index = 0; index < selects.length; index++) {
+						const select = selects[index].querySelector('select');
+						flsModules.select.selectBuild(select);
+					}
+				}
+			}
+		}, 0);
+	},
+	emailTest(formRequiredItem) {
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(formRequiredItem.value);
+	}
+}
+
+function getFormData(form) {
+	var elements = form.elements;
+	var honeypot;
+
+	var fields = Object.keys(elements).filter(function (k) {
+		if (elements[k].name === "honeypot") {
+			honeypot = elements[k].value;
+			return false;
+		}
+		return true;
+	}).map(function (k) {
+		if (elements[k].name !== undefined) {
+			return elements[k].name;
+			// special case for Edge's html collection
+		} else if (elements[k].length > 0) {
+			return elements[k].item(0).name;
+		}
+	}).filter(function (item, pos, self) {
+		return self.indexOf(item) == pos && item;
+	});
+
+	var formData = {};
+	fields.forEach(function (name) {
+		var element = elements[name];
+
+		// singular form elements just have one value
+		formData[name] = element.value;
+
+		// when our element has multiple items, get their values
+		if (element.length) {
+			var data = [];
+			for (var i = 0; i < element.length; i++) {
+				var item = element.item(i);
+				if (item.checked || item.selected) {
+					data.push(item.value);
+				}
+			}
+			formData[name] = data.join(', ');
+		}
+	});
+
+	// add form-specific values into the data
+	formData.formDataNameOrder = JSON.stringify(fields);
+	formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+	formData.formGoogleSendEmail
+		= form.dataset.email || ""; // no email by default
+
+	return { data: formData, honeypot: honeypot };
+}
+/* Отправка форм */
+export function formSubmit(options = { validate: true }) {
+	const forms = document.forms;
+	if (forms.length) {
+		for (const form of forms) {
+			form.addEventListener('submit', function (e) {
+				const form = e.target;
+				formSubmitAction(form, e);
+			});
+			form.addEventListener('reset', function (e) {
+				const form = e.target;
+				formValidate.formClean(form);
+			});
+		}
+	}
+	async function formSubmitAction(form, e) {
+		const error = !form.hasAttribute('data-no-validate') ? formValidate.getErrors(form) : 0;
+		if (error === 0) {
+			const ajax = form.hasAttribute('data-ajax');
+			if (ajax) { // Если режим ajax
+				e.preventDefault();
+				const formAction = form.getAttribute('action') ? form.getAttribute('action').trim() : '#';
+				const formMethod = form.getAttribute('method') ? form.getAttribute('method').trim() : 'GET';
+				var formData = getFormData(form);
+
+				var data = formData.data;
+				let appLink = "https://script.google.com/macros/s/AKfycbzT_YETRWRVe1Mqauy7huhH4s-n-duBK6Yttj8VXKwYZDiPiyc/exec";
+				var url = appLink;
+				var xhr = new XMLHttpRequest();
+
+				xhr.open('POST', url);
+				xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+				xhr.onreadystatechange = function () {
+					if (xhr.status === 200) {
+						form.classList.remove('_sending');
+						formSent(form);
+					}
+					else {
+						alert("Ошибка");
+						form.classList.remove('_sending');
+					}
+				};
+				// url encode form data for sending as post data
+				var encoded = Object.keys(data).map(function (k) {
+					return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+				}).join('&');
+				xhr.send(encoded);
+
+
+				form.classList.add('_sending');
+
+
+			} else if (form.hasAttribute('data-dev')) {	// Если режим разработки
+				e.preventDefault();
+				formSent(form);
+			}
+		} else {
+			e.preventDefault();
+			const formError = form.querySelector('._form-error');
+			if (formError && form.hasAttribute('data-goto-error')) {
+				gotoBlock(formError, true, 1000);
+			}
+		}
+	}
+	// Действия после отправки формы
+	function formSent(form) {
+		// Создаем событие отправки формы
+		document.dispatchEvent(new CustomEvent("formSent", {
+			detail: {
+				form: form
+			}
+		}));
+		// Показываем попап, если подключен модуль попапов 
+		// и для формы указана настройка
+		setTimeout(() => {
+			if (flsModules.popup) {
+				const popup = form.dataset.popupMessage;
+				popup ? flsModules.popup.open(popup) : null;
+			}
+		}, 0);
+		// Очищаем форму
+		formValidate.formClean(form);
+		// Сообщаем в консоль
+		alert('Отправлено!');
+		formLogging(`Форма отправлена!`);
+	}
+	function formLogging(message) {
+		FLS(`[Формы]: ${message}`);
+	}
+}
+/* Модуь формы "колличество" */
+export function formQuantity() {
+	document.addEventListener("click", function (e) {
+		let targetElement = e.target;
+		if (targetElement.closest('.quantity__button')) {
+			let value = parseInt(targetElement.closest('.quantity').querySelector('input').value);
+			if (targetElement.classList.contains('quantity__button_plus')) {
+				value++;
+			} else {
+				--value;
+				if (value < 1) value = 1;
+			}
+			targetElement.closest('.quantity').querySelector('input').value = value;
+		}
+	});
+}
+/* Модуь звездного рейтинга */
+export function formRating() {
+	const ratings = document.querySelectorAll('.rating');
+	if (ratings.length > 0) {
+		initRatings();
+	}
+	// Основная функция
+	function initRatings() {
+		let ratingActive, ratingValue;
+		// "Бегаем" по всем рейтингам на странице
+		for (let index = 0; index < ratings.length; index++) {
+			const rating = ratings[index];
+			initRating(rating);
+		}
+		// Инициализируем конкретный рейтинг
+		function initRating(rating) {
+			initRatingVars(rating);
+
+			setRatingActiveWidth();
+
+			if (rating.classList.contains('rating_set')) {
+				setRating(rating);
+			}
+		}
+		// Инициализайция переменных
+		function initRatingVars(rating) {
+			ratingActive = rating.querySelector('.rating__active');
+			ratingValue = rating.querySelector('.rating__value');
+		}
+		// Изменяем ширину активных звезд
+		function setRatingActiveWidth(index = ratingValue.innerHTML) {
+			const ratingActiveWidth = index / 0.05;
+			ratingActive.style.width = `${ratingActiveWidth}%`;
+		}
+		// Возможность указать оценку 
+		function setRating(rating) {
+			const ratingItems = rating.querySelectorAll('.rating__item');
+			for (let index = 0; index < ratingItems.length; index++) {
+				const ratingItem = ratingItems[index];
+				ratingItem.addEventListener("mouseenter", function (e) {
+					// Обновление переменных
+					initRatingVars(rating);
+					// Обновление активных звезд
+					setRatingActiveWidth(ratingItem.value);
+				});
+				ratingItem.addEventListener("mouseleave", function (e) {
+					// Обновление активных звезд
+					setRatingActiveWidth();
+				});
+				ratingItem.addEventListener("click", function (e) {
+					// Обновление переменных
+					initRatingVars(rating);
+
+					if (rating.dataset.ajax) {
+						// "Отправить" на сервер
+						setRatingValue(ratingItem.value, rating);
+					} else {
+						// Отобразить указанную оцнку
+						ratingValue.innerHTML = index + 1;
+						setRatingActiveWidth();
+					}
+				});
+			}
+		}
+		async function setRatingValue(value, rating) {
+			if (!rating.classList.contains('rating_sending')) {
+				rating.classList.add('rating_sending');
+
+				// Отправика данных (value) на сервер
+				let response = await fetch('rating.json', {
+					method: 'GET',
+
+					//body: JSON.stringify({
+					//	userRating: value
+					//}),
+					//headers: {
+					//	'content-type': 'application/json'
+					//}
+
+				});
+				if (response.ok) {
+					const result = await response.json();
+
+					// Получаем новый рейтинг
+					const newRating = result.newRating;
+
+					// Вывод нового среднего результата
+					ratingValue.innerHTML = newRating;
+
+					// Обновление активных звезд
+					setRatingActiveWidth();
+
+					rating.classList.remove('rating_sending');
+				} else {
+					alert("Ошибка");
+
+					rating.classList.remove('rating_sending');
+				}
+			}
+		}
+	}
+}
